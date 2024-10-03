16cf5465b9f957e45f4987ae1a238be355d61ca7
sobolev404 <goshasobolev2003@gmail.com>
2024-10-03 19:08:02 +0300

    feat: add btns for adding and removing pets in popups, and styles for them

--- src/components/SectionFriends/PetPopup.jsx
+++ src/components/SectionFriends/PetPopup.jsx
@@ -1,47 +1,84 @@
 import { useContext } from "react";
-import { AuthContext } from "../AuthContext"
-
+import { AuthContext } from "../AuthContext";
+import { useLocation } from "react-router-dom";
 
 export default function PetPopup({ pet, closePopup }) {
-  const {user, addPetToUser} = useContext(AuthContext)
+  const location = useLocation();
+
+  const { user, addPetToUser } = useContext(AuthContext);
+
 
   function convertMonthsToAge(months) {
     const years = Math.floor(months / 12);
     const remainingMonths = months % 12;
-  
+
     const yearsLabel = years > 1 ? "years" : "year";
     const monthsLabel = remainingMonths > 1 ? "months" : "month";
-  
+
     let result = "";
     if (years > 0) {
       result += `${years} ${yearsLabel}`;
     }
-  
+
     if (remainingMonths > 0) {
       if (years > 0) result += " "; // добавить пробел, если есть и годы
       result += `${remainingMonths} ${monthsLabel}`;
     }
-  
+
     return result || "0 months"; // если месяцев 0
   }
   return (
     <div onClick={(e) => e.stopPropagation()} className="popup-content">
+      <div className="popup-top">
+        <img src={pet.img} alt={pet.name} className="popup-img" />
+        <div className="popup-text">
+          <h3 className="popup-name">{pet.name}</h3>
+          <p className="popup-type">{convertMonthsToAge(pet.age)}</p>
+          <p className="popup-type">{pet.breed}</p>
+          <p className="popup-desc">{pet.description}</p>
+          <ul className="popup-qualities">
+            <li>
+              <b>Inoculations:</b>
+              {pet.inoculations.join(", ")}
+            </li>
+            <li>
+              <b>Diseases:</b>
+              {pet.diseases.join(", ")}
+            </li>
+            <li>
+              <b>Parasites:</b>
+              {pet.parasites.join(", ")}
+            </li>
+          </ul>
+        </div>
+      </div>
       <button className="popup-btn" onClick={closePopup}>
         &#x2715;
       </button>
-      <img src={pet.img} alt={pet.name} className="popup-img" />
-      <div className="popup-text">
-        <h3 className="popup-name">{pet.name}</h3>
-        <p className="popup-type">{convertMonthsToAge(pet.age)}</p>
-        <p className="popup-type">{pet.breed}</p>
-        <p className="popup-desc">{pet.description}</p>
-        <ul className="popup-qualities">
-            <li><b>Inoculations:</b>{pet.inoculations.join(", ")}</li>
-            <li><b>Diseases:</b>{pet.diseases.join(", ")}</li>
-            <li><b>Parasites:</b>{pet.parasites.join(", ")}</li>
-        </ul>
-      </div>
-      {user && <button onClick={()=>addPetToUser(pet)}>Add</button>}
+      {(user && (location.pathname !== "/profile")) && (
+        <button
+          className="popup-btn-add"
+          onClick={() => {
+            addPetToUser(pet);
+            closePopup();
+            alert(`${pet.name} was added to your wishlist`);
+          }}
+        >
+          Add to a wishlist
+        </button>
+      )}
+      {(user && (location.pathname === "/profile")) && (
+        <button
+          className="popup-btn-add popup-btn-delete"
+          onClick={() => {
+            addPetToUser(pet);
+            closePopup();
+            alert(`${pet.name} was removed from your wishlist`);
+          }}
+        >
+          Remove from a wishlist
+        </button>
+      )}
     </div>
   );
 }
