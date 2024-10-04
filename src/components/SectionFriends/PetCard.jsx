7ce33548c2bb0683512251ce4d1c05842c497927
sobolev404 <goshasobolev2003@gmail.com>
2024-10-04 14:51:07 +0300

    Updates

--- src/components/SectionFriends/PetCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function PetCard({ imgUrl, imgAlt, petName, onClick }) {
-  return (
-    <div onClick={onClick} className="friend-item">
-      <img src={imgUrl} alt={imgAlt} />
-      <p className="friend-name">{petName}</p>
-      <button className="btn-more">Learn more</button>
-    </div>
-  );
-}
